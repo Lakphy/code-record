2c71ce2b90f968b142f5df8411e519f2b7ea876e
lakphy <lakphycn@gmail.com>
2023-08-13 11:45:42 +0800

    230813

--- src/lru-cache.js
+++ src/lru-cache.js
@@ -4,15 +4,33 @@
 var LRUCache = function (capacity) {
   this.capacity = capacity;
   this.size = 0;
-  this.head = null;
-  this.tail = null;
+  this.head = new LRUNode(null, null, null, null);
+  this.tail = new LRUNode(null, null, null, null);
+  this.head.next = this.tail;
+  this.tail.prev = this.head;
+  this.hash = Object.create(null);
 };
 
 /**
  * @param {number} key
  * @return {number}
  */
-LRUCache.prototype.get = function (key) {};
+LRUCache.prototype.get = function (key) {
+  const last = this.hash[key];
+  if (last) {
+    const prev = last.prev;
+    const next = last.next;
+    prev.next = next;
+    next.prev = prev;
+    const newest = this.tail.prev;
+    newest.next = last;
+    last.prev = newest;
+    last.next = this.tail;
+    this.tail.prev = last;
+    return last.value;
+  }
+  return -1;
+};
 
 /**
  * @param {number} key
@@ -20,29 +38,43 @@ LRUCache.prototype.get = function (key) {};
  * @return {void}
  */
 LRUCache.prototype.put = function (key, value) {
-  if (!this.head) {
-    this.head = new LRUNode(key, value, null);
-    this.tail = this.head;
-    this.size++;
-    return;
+  // console.log(this.hash);
+  // console.log(this.head);
+  const last = this.hash[key];
+  if (last) {
+    last.value = value;
+    const prev = last.prev;
+    const next = last.next;
+    prev.next = next;
+    next.prev = prev;
+    const newest = this.tail.prev;
+    newest.next = last;
+    last.prev = newest;
+    last.next = this.tail;
+    this.tail.prev = last;
   } else {
-    let cur = this.head;
-    while (cur) {
-      if (cur.key === key) {
-        cur.value = value;
-
-        return;
-      }
-      cur = cur.next;
+    if (this.size === this.capacity) {
+      const del = this.head.next;
+      const next = del.next;
+      this.head.next = next;
+      next.prev = this.head;
+      delete this.hash[del.key];
+      this.size--;
     }
+    const newest = this.tail.prev;
+    const insert = new LRUNode(key, value, this.tail.prev, this.tail);
+    this.hash[key] = insert;
+    newest.next = insert;
+    this.tail.prev = insert;
+    this.size++;
   }
 };
 
-function LRUNode(key, value, prev) {
+function LRUNode(key, value, prev, next) {
   this.key = key;
   this.value = value;
   this.prev = prev;
-  this.next = null;
+  this.next = next;
 }
 
 /**
